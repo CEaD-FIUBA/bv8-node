84ad4f8884a6e6d21267304fe94fbbd9efc8f5ed
crisnicogonzalez <cristian3629@gmail.com>
2019-02-04 22:31:28 -0300

    Update server call script

--- public/serverCalls.js
+++ public/serverCalls.js
@@ -1,26 +1,41 @@
 var port = 443;
 
-$(document).ready(function(){
-	console.log('ready');
-	serverCall();
+$(document).ready(function () {
+    console.log('ready');
+    serverCall();
 });
 
 function serverCall() {
     var xhttp = new XMLHttpRequest();
-    xhttp.open('GET', 'http://157.92.49.65:'+port+'/'+videoId, true);
+    xhttp.open('GET', `http://bbv8.herokuapp.com/videos/${videoId}`, true);
     //xhttp.setRequestHeader("Content-type", "application/json");
-		//xhttp.setRequestHeader('Access-Control-Allow-Headers', '*');
-		//xhttp.setRequestHeader("crossDomaintrue",true);
-    xhttp.onreadystatechange = function() {
-        if (xhttp.readyState == 4 && xhttp.status == 200){
-	    		console.log('dsdas:'+xhttp.responseText);
-					var responseJSON = JSON.parse(xhttp.responseText);
-					parserCaption(responseJSON['caption']);
-				}
-			console.log('onreadystatechange: '+xhttp.readyState+'status: '+xhttp.status);
+    //xhttp.setRequestHeader('Access-Control-Allow-Headers', '*');
+    //xhttp.setRequestHeader("crossDomaintrue",true);
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            console.log('dsdas:' + xhttp.responseText);
+            var responseJSON = JSON.parse(xhttp.responseText);
+            getCaptions(responseJSON['id_caption']);
+        }
+        console.log('onreadystatechange: ' + xhttp.readyState + 'status: ' + xhttp.status);
     }
     xhttp.send();
-    console.log('send')
+    console.log('GET captionId')
     //var response = JSON.parse(xhttp.responseText);
+}
 
+
+function getCaptions(idCaption) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open('GET', `https://bbv8-py.herokuapp.com/captions/${idCaption}`, true);
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            console.log('dsdas:' + xhttp.responseText);
+            var responseJSON = JSON.parse(xhttp.responseText);
+            parseCaption(responseJSON['id_caption']);
+        }
+        console.log('onreadystatechange: ' + xhttp.readyState + 'status: ' + xhttp.status);
+    }
+    xhttp.send();
+    console.log('getCaption')
 }
