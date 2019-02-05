d5d1f1da163d99af5c5c6185a09b964e15ee0eb8
crisnicogonzalez <cristian3629@gmail.com>
2019-02-04 23:24:55 -0300

    refactor caption parse

--- public/serverCalls.js
+++ public/serverCalls.js
@@ -1,4 +1,58 @@
-var port = 443;
+var captions = [];
+var timesOfChapters = [];
+var chapters = [];
+
+const TOPIC_TEXT = "Tema"
+
+function isTopicText(text) {
+    const idx = text.search(TOPIC_TEXT);
+    return idx != -1;
+}
+
+function parserCaption(lines) {
+    lines.forEach(line => {
+        const text = line['caption']
+        const time = line['time']
+        const htmlLine = $("<p></p>")
+        htmlLine.val(time)
+        if (isTopicText(text)) {
+            htmlLine.addClass("chapter");
+            $("#chapters").append(line);
+            timesOfChapters.push(time);
+            chapters.push(line)
+        } else {
+            htmlLine.addClass("line")
+            $("#caption").append(line);
+            captions.push(line)
+        }
+    });
+    // var array = caption_string.split(",");
+    // var len = array.length;
+    // for (var i = 0; i < len - 1; i = i + 2) {
+    //     var time = array[i];
+    //     var text = array[i + 1];
+    //     var line = $("<p></p>");
+    //     line.val(time);
+    //     //Aca me fijo si mi caption es un indice o un caption
+    //     var n = text.search("Tema:");
+    //     //Si no es un indice
+    //     if (n == -1) {
+    //         line.text(text);
+    //         line.addClass('line');
+    //         $("#caption").append(line);
+    //         captions.push(line);
+    //     } else {
+    //         var chapter_name = text.split(":")[1];
+    //         line.text(chapter_name);
+    //         //console.log("chapter_name:"+chapter_name);
+    //         line.addClass('chapter');
+    //         $("#chapters").append(line);
+    //         timesOfChapters.push(time);
+    //         chapters.push(line);
+    //     }
+    //     console.log('text: ' + text + 'time: ' + time);
+    // }
+}
 
 $(document).ready(function () {
     console.log('ready');
@@ -26,12 +80,12 @@ function serverCall() {
 
 
 function getCaptions(idCaption) {
-    var xhttp = new XMLHttpRequest();
+    const xhttp = new XMLHttpRequest();
     xhttp.open('GET', `https://bbv8-py.herokuapp.com/captions/${idCaption}`, true);
     xhttp.onreadystatechange = function () {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
-            console.log('dsdas:' + xhttp.responseText);
-            var responseJSON = unescape(JSON.parse(xhttp.responseText));
+            const responseJSON = unescape(JSON.parse(xhttp.responseText));
+            console.log('dsdas:' + responseJSON);
             parseCaption(responseJSON['id_caption']);
         }
         console.log('onreadystatechange: ' + xhttp.readyState + 'status: ' + xhttp.status);
