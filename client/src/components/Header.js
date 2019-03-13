304203ef8c46e7a7d55788e6a1580fdf8da72fd0
crisnicogonzalez <cristian3629@gmail.com>
2019-03-13 20:19:15 -0300

    Add button to slide themes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -12,43 +12,61 @@ export default class Header extends Component {
   render() {
     return (
       <div style={{ marginBottom: '20px' }}>
-        <Carousel style={{ background: '#F5F5F5' }} afterChange={this.onChange} >
-          <div>
-            <Button type="primary" style={{ background: '#CFD2D4', borderColor: '#CFD2D4', color: 'black', marginRight: '40px' }}>
-              Tema 1
+        <Row type="flex" justify="end">
+          <div style={{ marginBottom: '20px', fontWeight: 'bolder', borderBottom: '3px solid #28ABE0', paddingBottom: '3px' }}>Titulo video - Estudios de perdidas y corrección del factor de potencia</div>
+        </Row>
+        <Row type="flex" justify="start">
+          <div style={{ marginBottom: '20px', fontWeight: 'bolder', borderBottom: '3px solid #28ABE0', paddingBottom: '3px' }}>Temario</div>
+        </Row>
+        <Row>
+          <Col span={1}>
+            <Button icon="left" style={{ background: '#F5F5F5', borderColor: '#F5F5F5', color: '#28ABE0', padding: '0px 12px' }} />
+          </Col>
+          <Col span={22}>
+            <Carousel style={{ background: '#F5F5F5' }} afterChange={this.onChange} >
+              <div>
+                <Button type="primary" style={{ background: '#CFD2D4', borderColor: '#CFD2D4', color: 'black', marginRight: '40px' }}>
+                  Tema 1
             </Button>
-            <div style={{ width: '2px', height: '25px', display: 'inline-block', background: '#28ABE0', position: 'absolute', marginTop: '27px' }}></div>
-            <Button type="primary" style={{ background: '#F5F5F5', borderColor: '#F5F5F5', color: 'black', margin: '0px 40px' }}>
-              Tema 2
+                <div style={{ width: '2px', height: '25px', display: 'inline-block', background: '#28ABE0', position: 'absolute', marginTop: '27px' }}></div>
+                <Button type="primary" style={{ background: '#F5F5F5', borderColor: '#F5F5F5', color: 'black', margin: '0px 40px' }}>
+                  Tema 2
             </Button>
-            <div style={{ width: '2px', height: '25px', display: 'inline-block', background: '#28ABE0', position: 'absolute', marginTop: '27px' }}></div>
-            <Button type="primary" style={{ background: '#F5F5F5', borderColor: '#F5F5F5', color: 'black', margin: '0px 40px' }}>
-              Tema 3
+                <div style={{ width: '2px', height: '25px', display: 'inline-block', background: '#28ABE0', position: 'absolute', marginTop: '27px' }}></div>
+                <Button type="primary" style={{ background: '#F5F5F5', borderColor: '#F5F5F5', color: 'black', margin: '0px 40px' }}>
+                  Tema 3
             </Button>
-            <div style={{ width: '2px', height: '25px', display: 'inline-block', background: '#28ABE0', position: 'absolute', marginTop: '27px' }}></div>
-            <Button type="primary" style={{ background: '#F5F5F5', borderColor: '#F5F5F5', color: 'black', margin: '0px 40px' }}>
-              Tema 4
+                <div style={{ width: '2px', height: '25px', display: 'inline-block', background: '#28ABE0', position: 'absolute', marginTop: '27px' }}></div>
+                <Button type="primary" style={{ background: '#F5F5F5', borderColor: '#F5F5F5', color: 'black', margin: '0px 40px' }}>
+                  Tema 4
             </Button>
-          </div>
-          <div>
-            <Button type="primary" style={{ background: '#CFD2D4', borderColor: '#CFD2D4', color: 'black', marginRight: '40px' }}>
-              Tema 1
+              </div>
+              <div>
+                <Button type="primary" style={{ background: '#CFD2D4', borderColor: '#CFD2D4', color: 'black', marginRight: '40px' }}>
+                  Tema 1
             </Button>
-            <div style={{ width: '2px', height: '25px', display: 'inline-block', background: '#28ABE0', position: 'absolute', marginTop: '27px' }}></div>
-            <Button type="primary" style={{ background: '#F5F5F5', borderColor: '#F5F5F5', color: 'black', margin: '0px 40px' }}>
-              Tema 2
+                <div style={{ width: '2px', height: '25px', display: 'inline-block', background: '#28ABE0', position: 'absolute', marginTop: '27px' }}></div>
+                <Button type="primary" style={{ background: '#F5F5F5', borderColor: '#F5F5F5', color: 'black', margin: '0px 40px' }}>
+                  Tema 2
             </Button>
-            <div style={{ width: '2px', height: '25px', display: 'inline-block', background: '#28ABE0', position: 'absolute', marginTop: '27px' }}></div>
-            <Button type="primary" style={{ background: '#F5F5F5', borderColor: '#F5F5F5', color: 'black', margin: '0px 40px' }}>
-              Tema 3
+                <div style={{ width: '2px', height: '25px', display: 'inline-block', background: '#28ABE0', position: 'absolute', marginTop: '27px' }}></div>
+                <Button type="primary" style={{ background: '#F5F5F5', borderColor: '#F5F5F5', color: 'black', margin: '0px 40px' }}>
+                  Tema 3
             </Button>
-            <div style={{ width: '2px', height: '25px', display: 'inline-block', background: '#28ABE0', position: 'absolute', marginTop: '27px' }}></div>
-            <Button type="primary" style={{ background: '#F5F5F5', borderColor: '#F5F5F5', color: 'black', margin: '0px 40px' }}>
-              Tema 4
+                <div style={{ width: '2px', height: '25px', display: 'inline-block', background: '#28ABE0', position: 'absolute', marginTop: '27px' }}></div>
+                <Button type="primary" style={{ background: '#F5F5F5', borderColor: '#F5F5F5', color: 'black', margin: '0px 40px' }}>
+                  Tema 4
             </Button>
-          </div>
-        </Carousel>
-      </div>
+              </div>
+            </Carousel>
+
+          </Col>
+          <Col span={1}>
+            <Button icon="right" style={{ background: '#F5F5F5', borderColor: '#F5F5F5', color: '#28ABE0', padding: '0px 12px' }} />
+          </Col>
+        </Row>
+
+      </div >
     );
   }
 } 
