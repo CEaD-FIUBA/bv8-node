d39d53615d0c8ba7f4903be97adfc581bc6439b7
crisnicogonzalez <cristian3629@gmail.com>
2019-03-17 23:13:31 -0300

    render body

--- front/src/components/Body.js
+++ front/src/components/Body.js
@@ -3,17 +3,19 @@ import Caption from './Caption'
 
 
 export default class Body extends Component {
+
+  createRenderContent = () => {
+    return this.props.captions.map((caption) => {
+      return <Caption
+        text={caption.caption}
+        key={caption.time}
+      />
+    })
+  }
+
   render() {
     return <div className='body'>
-      <Caption
-        text={'Buenos Días mi nombre es Luis Donseli yo soy docente del departamento de Electrotenia'}
-      />
-      <Caption
-        text='de la Facultad de Ingeniería de la UBA'
-      />
-      <Caption
-        text='de la Facultad de Ingeniería de la UBA'
-      />
+      {this.createRenderContent()}
     </div>
   }
 
