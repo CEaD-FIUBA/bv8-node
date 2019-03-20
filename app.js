857251ec445de697fd5cdc747efe84cbd84b5d93
crisnicogonzalez <cristian3629@gmail.com>
2019-03-20 18:34:13 -0300

    Add deploy configuration

--- app.js
+++ app.js
@@ -43,6 +43,22 @@ app.get('/videos/:id', function (req, res) {
         })
 })
 
+//Configuration
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'front/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'front/build', 'index.html'));
+    });
+}
+
+
 
 
 
